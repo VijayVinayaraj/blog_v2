3dbc837a94d2226ef77b005f36019ae60fb335de
VijayVinayaraj <vijayvinayaraj@gmail.com>
2024-05-26 12:13:38 +0530

    add themeui project wide

--- src/templates/BlogPage.tsx
+++ src/templates/BlogPage.tsx
@@ -1,7 +1,7 @@
 import { PageProps } from 'gatsby'
 import React from 'react'
-import parse from 'html-react-parser'
-import { Text } from 'theme-ui'
+import parse, { Element, HTMLReactParserOptions, domToReact, DOMNode } from 'html-react-parser'
+import { Box, Flex, Text } from 'theme-ui'
 export type BlogPost = {
   id: string
   title: string
@@ -14,11 +14,41 @@ export default function BlogPage({ pageContext }: PageProps<{}, BlogPost>) {
   console.log('data', pageContext)
   const { title, date, contentHtml } = pageContext
   const dateString = new Date(date).toISOString()
+
+  const options: HTMLReactParserOptions = {
+    replace(domNode) {
+      const domNodeWithType = domNode as Element
+
+      if (domNodeWithType.name === 'div' && domNodeWithType.attribs.class === 'content') {
+        return (
+          <Flex sx={{ flexDirection: 'column', justifyContent: 'center' }}>
+            {domToReact(domNodeWithType.children as DOMNode[], options)}
+          </Flex>
+        )
+      }
+
+      if (domNodeWithType.name === 'img') {
+        return (
+          <Box
+            as="img"
+            sx={{
+              maxWidth: '100%',
+              height: 'auto',
+              margin: '0 auto',
+              display: 'block',
+            }}
+          />
+        )
+      }
+
+      // Continue parsing other elements normally
+      return domNodeWithType
+    },
+  }
+
   return (
-    <div>
-      <Text>{title}</Text>
-      <Text>{dateString}</Text>
-      {parse(contentHtml)}
-    </div>
+    <Flex sx={{ justifyContent: 'center', flexWrap: 'wrap', marginX: '100px' }}>
+      {parse(contentHtml, options)}
+    </Flex>
   )
 }
